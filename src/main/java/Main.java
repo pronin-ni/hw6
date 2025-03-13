a6832c370f94c2be36ac584b8f9c53c439ca4909
npronin <noxywille@yandex.ru>
2025-03-13 22:49:10 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        float f;
+        double d;
+        a = 10;
+        b = 20;
+        c = 20;
+        f = 2.5F;
+
+        System.out.println("Арифметические операции");
+        System.out.println("Результат сложения a + b = " + (a + b));
+        System.out.println("Результат вычитания b - a = " + (b - a));
+        System.out.println("Результат умножения a * b = " + (a * b));
+        System.out.println("Результат деления b / a = " + (b / a));
+        System.out.println("Результат сложения a + f = " + (a + f));
+        System.out.println("Результат деления a / f = " + (a / f) + "\n");
+
+        System.out.println("Логические операции");
+        System.out.println("b больше чем a? " + (a > b));
+        System.out.println("b меньше или равно чем a? " + (a <= b));
+        System.out.println("b равно с? " + (b == c));
+        System.out.println("b больше или равно с? " + (b >= c) + "\n");
+        System.out.println("Переполнение при арифметической операции");
+        f = Float.MAX_VALUE;
+        d = Double.MAX_VALUE;
+
+        System.out.println("Переполненное значение float: " + (f * 2));
+        System.out.println("Переполненное значение double: " + (d * 2));
+        System.out.println("Результат деления 0.0/0.0: " + (0.0 / 0.0));
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
